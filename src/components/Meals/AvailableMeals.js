cac7660dae2b716f78dc4d6fd6b1347779671c45
tugaysert <101674140+tugaysert@users.noreply.github.com>
2022-11-27 17:16:09 +0300

    add meal items

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,42 +1,51 @@
+import Card from "../UI/Card/Card";
 import classes from "./AvaliableMeals.module.css";
+import MealItem from "./MealItem/MealItem";
 
 const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-    },
-    {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-    },
-    {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
-    },
-    {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
-    },
-  ];
+  {
+    id: "m1",
+    name: "Sushi",
+    description: "Finest fish and veggies",
+    price: 22.99,
+  },
+  {
+    id: "m2",
+    name: "Schnitzel",
+    description: "A german specialty!",
+    price: 16.5,
+  },
+  {
+    id: "m3",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
+    price: 12.99,
+  },
+  {
+    id: "m4",
+    name: "Green Bowl",
+    description: "Healthy...and green...",
+    price: 18.99,
+  },
+];
 
 const AvaliableMeals = () => {
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    ></MealItem>
+  ));
 
-    const mealsList = DUMMY_MEALS.map(meal => (<li>{meal.name}</li>));
-
-    return <section className={classes.meals}>
-        <ul>
-            {mealsList}
-        </ul>
+  return (
+    <section className={classes.meals}>
+      <Card className={classes["meals ul"]}>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
-}
-
+  );
+};
 
 export default AvaliableMeals;
