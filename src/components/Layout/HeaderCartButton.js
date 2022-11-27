46c6dc70800f1d9236163c5c5ca88b2bd9892a10
tugaysert <101674140+tugaysert@users.noreply.github.com>
2022-11-27 20:55:53 +0300

    add context

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,22 @@
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
+import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCardItems = cartCtx.items.reduce((currentNumber, item) => {
+    return currentNumber + item.amount;
+  }, 0);
+
   return (
     <button type="submit" onClick={props.onClick} className={classes.button}>
       <span className={classes["icon"]}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes["badge"]}>3</span>
+      <span className={classes["badge"]}>{numberOfCardItems}</span>
     </button>
   );
 };
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,9 +1,9 @@
 import { Fragment } from "react";
 import classes from "./Modal.module.css";
-import  ReactDOM  from "react-dom";
+import ReactDOM from "react-dom";
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop} onClick = {props.onClose}/>;
+  return <div className={classes.backdrop} onClick={props.onClose} />;
 };
 
 const ModalOverlay = (props) => {
@@ -14,19 +14,27 @@ const ModalOverlay = (props) => {
   );
 };
 
-const portalElement = document.getElementById('overlays');
+const portalElement = document.getElementById("overlays");
 
 const Modal = (props) => {
-    /**if we would not be using the portal */
-/*     return <Fragment>
+  /**if we would not be using the portal */
+  /*     return <Fragment>
         <Backdrop/>
         <ModalOverlay>{props.children}</ModalOverlay>
     </Fragment> */
 
-    return <Fragment>
-        {ReactDOM.createPortal(<Backdrop onClose = {props.onClose}/>,portalElement )}
-        {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalElement
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
     </Fragment>
+  );
 };
 
 export default Modal;
